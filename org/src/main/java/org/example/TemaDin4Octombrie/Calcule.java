e7a7142624052f5dcb57f0705b909c76ab2ca5d0
burlacugeorge <burlacugeorge22@gmail.com>
2025-10-27 16:16:18 +0200

    Am adaugat tema "Calcule" din data de 4.10.2025

---
+++ org/src/main/java/org/example/TemaDin4Octombrie/Calcule.java
@@ -0,0 +1,21 @@
+package org.example.TemaDin4Octombrie;
+
+public class Calcule {
+    static void main() {
+
+        final int YEAR_OF_BIRTH = 1991;
+        long a = 1234567890;
+        long b = 1098765432;
+        long sum = a + b;
+        long prod = a * b;
+        double divizion = a / b;
+        long substration = a - b;
+        System.out.println(YEAR_OF_BIRTH);
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " * " + b + " = " + prod);
+        System.out.println(a + " / " + b + " = " + divizion);
+        System.out.println(a + " - " + b + " = " + substration);
+
+
+    }
+}
