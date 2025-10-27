03eb08e23e7899f5d412e80b957b6d97d8c5f8d5
burlacugeorge <burlacugeorge22@gmail.com>
2025-10-27 15:50:39 +0200

    Am adaugat tema "Concatenare" din data de 4.10.2025

---
+++ org/src/main/java/org/example/TemaDin4Octombrie/Concatenare.java
@@ -0,0 +1,13 @@
+package org.example.TemaDin4Octombrie;
+
+public class Concatenare {
+    static void main(String[] args) {
+
+        String firstName = "Burlacu";
+        String middleName = "George";
+        String lastName = "Nicolae";
+        System.out.println(firstName + " " + middleName + " " + lastName);
+
+    }
+
+}
